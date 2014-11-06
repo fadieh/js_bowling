823d9399ebb9e40258a4138e246236d654272e62
Fadie Hannona <fadiehannona@gmail.com>
2014-11-06 17:53:07 +0000

    Refactored

--- src/bowling.js
+++ src/bowling.js
@@ -4,23 +4,41 @@ function Bowling() {
   this.framesLeft = 10
   this.frameTurn = 2
   this.totalScore = 0
+  this.turn
 
 };
 
 Bowling.prototype.rollBall = function() {
   if (this.frameTurn > 0)
-  { this.frameTurn = this.frameTurn - 1 }
+  { this.nextFrameTurn() }
   if (this.frameTurn === 0)
-  { this.frameTurn = 2, this.framesLeft = this.framesLeft = this.framesLeft - 1}
+  { this.nextFrame() }
+};
+
+Bowling.prototype.nextFrame = function() {
+  this.frameTurn = 2
+  this.framesLeft = this.framesLeft = this.framesLeft - 1
+};
+
+Bowling.prototype.nextFrameTurn = function() {
+  this.frameTurn = this.frameTurn - 1
 };
 
 Bowling.prototype.numberOfPinsHit = function(turn) {
   var turn = turn || Math.floor((Math.random()*10) + 0);
-  this.pins = this.pins - turn
-  this.totalScore = this.totalScore + turn
+  this.updatePinsLeft(turn)
+  this.updateTotalScore(turn)
 };
 
 Bowling.prototype.checkAllPinsHit = function () {
   if ((this.pins == 0) && (this.frameTurn == 1))
-  { this.framesLeft = this.framesLeft - 1 }
+  { this.nextFrame() }
+};
+
+Bowling.prototype.updatePinsLeft = function(turn) {
+  this.pins = this.pins - turn
+};
+
+Bowling.prototype.updateTotalScore = function(turn) {
+  this.totalScore = this.totalScore + turn
 };
