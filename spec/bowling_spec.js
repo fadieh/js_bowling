a3bb61606ee2ac8ea4e9447ff9d6816b977a7379
Fadie Hannona <fadiehannona@gmail.com>
2014-11-06 16:55:35 +0000

    Each frame should have 2 turns

--- spec/bowling_spec.js
+++ spec/bowling_spec.js
@@ -12,4 +12,8 @@ it ('should have 10 frames', function(){
     expect(bowling.framesLeft).toEqual(10)
 });
 
+it ('each frame should have 2 turns', function(){
+    expect(bowling.frameTurn).toEqual(2)
+});
+
 });
--- src/bowling.js
+++ src/bowling.js
@@ -2,5 +2,6 @@ function Bowling() {
 
   this.pins = 10
   this.framesLeft = 10
+  this.frameTurn = 2
 
 };
