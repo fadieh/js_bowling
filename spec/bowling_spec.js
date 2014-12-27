cd68d8b4de6efefaaf01f1a46675bda2e9c9b716
Fadie Hannona <fadiehannona@gmail.com>
2014-12-27 22:32:41 +0000

    starting again with a better design!

--- spec/bowling_spec.js
+++ spec/bowling_spec.js
@@ -1,72 +1,91 @@
-describe ('Bowling', function(){
+describe ('Bowling', function() {
 
   beforeEach(function(){
     bowling = new Bowling
   });
 
-it ('should initialise with 10 pins', function(){
-    expect(bowling.pins).toEqual(10)
+  it('score should contain 10 frames', function(){
+    expect(bowling.score.length).toEqual(10)
+  })
+
+  it('should randomly generate a number for a bowl and write to the score array', function(){
+    bowling.bowl(7)
+    expect(bowling.score[0][0]).toEqual(7)
+  });
+
 });
 
-it ('should initialise with 10 frames remaining', function(){
-    expect(bowling.framesLeft).toEqual(10)
-});
 
-it ('each frame should have 2 turns', function(){
-    expect(bowling.frameTurn).toEqual(2)
-});
 
-it ('should be able to take a shot and move to the next turn', function(){
-   bowling.nextTurn()
-   expect(bowling.frameTurn).toEqual(1)
-   expect(bowling.framesLeft).toEqual(10)
-});
+// describe ('Bowling', function(){
 
-it ('should move to next frame, once 2 turns have been taken', function(){
-  bowling.frameTurn = 1
-  bowling.nextTurn()
-  expect(bowling.framesLeft).toEqual(9)
-  expect(bowling.frameTurn).toEqual(2)
-});
+//   beforeEach(function(){
+//     bowling = new Bowling
+//   });
 
-it ('a bowl should generate a number between 0-10', function(){
-  bowling.bowl()
-  expect(bowling.pins).toBeGreaterThan(-1)
-  expect(bowling.pins).toBeLessThan(11)
-});
-
-it ('when pins are zero with 1 turn left, move onto next frame', function(){
-  bowling.pins = 0
-  bowling.frameTurn = 1
-  bowling.framesLeft = 10
-  bowling.checkAllPinsHit()
-  expect(bowling.framesLeft).toEqual(9)
-});
-
-it ('pins should not go below zero', function(){
-  bowling.pins = -1
-  bowling.checkAllPinsHit()
-  expect(bowling.pins).toEqual(0)
-});
-
-it ('should have a total score when initialised', function(){
-  expect(bowling.totalScore).toEqual(0)
-});
-
-it ('should add to total score after each turn', function(){
-  bowling.bowl(9)
-  expect(bowling.totalScore).toBeGreaterThan(0)
-});
-
-it ('should take a record of the previous shot', function(){
-  bowling.bowl(9)
-  expect(bowling.lastScore).toEqual(9)
-  bowling.bowl(7)
-  expect(bowling.lastScore).toEqual(7)
-});
-
-// it ('should account for a strike', function(){
-
+// it ('should initialise with 10 pins', function(){
+//     expect(bowling.pins).toEqual(10)
 // });
 
-});
+// it ('should initialise with 10 frames remaining', function(){
+//     expect(bowling.framesLeft).toEqual(10)
+// });
+
+// it ('each frame should have 2 turns', function(){
+//     expect(bowling.frameTurn).toEqual(2)
+// });
+
+// it ('should be able to take a shot and move to the next turn', function(){
+//    bowling.nextTurn()
+//    expect(bowling.frameTurn).toEqual(1)
+//    expect(bowling.framesLeft).toEqual(10)
+// });
+
+// it ('should move to next frame, once 2 turns have been taken', function(){
+//   bowling.frameTurn = 1
+//   bowling.nextTurn()
+//   expect(bowling.framesLeft).toEqual(9)
+//   expect(bowling.frameTurn).toEqual(2)
+// });
+
+// it ('a bowl should generate a number between 0-10', function(){
+//   bowling.bowl()
+//   expect(bowling.pins).toBeGreaterThan(-1)
+//   expect(bowling.pins).toBeLessThan(11)
+// });
+
+// it ('when pins are zero with 1 turn left, move onto next frame', function(){
+//   bowling.pins = 0
+//   bowling.frameTurn = 1
+//   bowling.framesLeft = 10
+//   bowling.checkAllPinsHit()
+//   expect(bowling.framesLeft).toEqual(9)
+// });
+
+// it ('pins should not go below zero', function(){
+//   bowling.pins = -1
+//   bowling.checkAllPinsHit()
+//   expect(bowling.pins).toEqual(0)
+// });
+
+// it ('should have a total score when initialised', function(){
+//   expect(bowling.totalScore).toEqual(0)
+// });
+
+// it ('should add to total score after each turn', function(){
+//   bowling.bowl(9)
+//   expect(bowling.totalScore).toBeGreaterThan(0)
+// });
+
+// it ('should take a record of the previous shot', function(){
+//   bowling.bowl(9)
+//   expect(bowling.lastScore).toEqual(9)
+//   bowling.bowl(7)
+//   expect(bowling.lastScore).toEqual(7)
+// });
+
+// // it ('should account for a strike', function(){
+
+// // });
+
+// });
--- src/bowling.js
+++ src/bowling.js
@@ -1,53 +1,67 @@
 function Bowling() {
 
-  this.pins = 10
-  this.framesLeft = 10
-  this.frameTurn = 2
-  this.totalScore = 0
-  this.lastScore = 0
+  this.score = [[],[],[],[],[],[],[],[],[],[]]
+  this.frameNumber = 1
+  this.frameTurn = 1
 
 };
 
 Bowling.prototype.bowl = function(turn) {
-  var turn = turn || Math.floor((Math.random()*10) + 0);
-  this.updatePinsLeft(turn)
-  this.updateTotalScore(turn)
-  this.checkAllPinsHit()
-  this.nextTurn()
-  this.lastScore = turn
+  this.score[this.frameNumber - 1][this.frameTurn - 1] = turn || Math.floor((Math.random()*10) + 0);
 };
 
-Bowling.prototype.nextTurn = function() {
-  if (this.frameTurn > 0)
-  { this.nextFrameTurn() }
-  if (this.frameTurn === 0)
-  { this.nextFrame() }
-};
 
-Bowling.prototype.checkAllPinsHit = function () {
-  if ((this.pins == 0) && (this.frameTurn == 1))
-  { this.nextFrame() }
-  else if (this.pins <= 0)
-  { this.revertToZero() }
-};
 
-Bowling.prototype.nextFrame = function() {
-  this.frameTurn = 2
-  this.framesLeft = this.framesLeft = this.framesLeft - 1
-};
+// function Bowling() {
 
-Bowling.prototype.nextFrameTurn = function() {
-  this.frameTurn = this.frameTurn - 1
-};
+//   this.pins = 10
+//   this.framesLeft = 10
+//   this.frameTurn = 2
+//   this.totalScore = 0
+//   this.lastScore = 0
 
-Bowling.prototype.updatePinsLeft = function(turn) {
-  this.pins = this.pins - turn
-};
+// };
 
-Bowling.prototype.updateTotalScore = function(turn) {
-  this.totalScore = this.totalScore + turn
-};
+// Bowling.prototype.bowl = function(turn) {
+//   var turn = turn || Math.floor((Math.random()*10) + 0);
+//   this.updatePinsLeft(turn)
+//   this.updateTotalScore(turn)
+//   this.checkAllPinsHit()
+//   this.nextTurn()
+//   this.lastScore = turn
+// };
 
-Bowling.prototype.revertToZero = function() {
-  this.pins = 0
-};
+// Bowling.prototype.nextTurn = function() {
+//   if (this.frameTurn > 0)
+//   { this.nextFrameTurn() }
+//   if (this.frameTurn === 0)
+//   { this.nextFrame() }
+// };
+
+// Bowling.prototype.checkAllPinsHit = function () {
+//   if ((this.pins == 0) && (this.frameTurn == 1))
+//   { this.nextFrame() }
+//   else if (this.pins <= 0)
+//   { this.revertToZero() }
+// };
+
+// Bowling.prototype.nextFrame = function() {
+//   this.frameTurn = 2
+//   this.framesLeft = this.framesLeft = this.framesLeft - 1
+// };
+
+// Bowling.prototype.nextFrameTurn = function() {
+//   this.frameTurn = this.frameTurn - 1
+// };
+
+// Bowling.prototype.updatePinsLeft = function(turn) {
+//   this.pins = this.pins - turn
+// };
+
+// Bowling.prototype.updateTotalScore = function(turn) {
+//   this.totalScore = this.totalScore + turn
+// };
+
+// Bowling.prototype.revertToZero = function() {
+//   this.pins = 0
+// };
