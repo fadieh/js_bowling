ced053a5e1708d1a11fcace1d7ad57e9c4a7e404
Fadie Hannona <fadiehannona@gmail.com>
2015-03-17 15:05:30 +0000

    start again

--- spec/bowling_spec.js
+++ spec/bowling_spec.js
@@ -8,57 +8,76 @@ describe ('Bowling', function() {
     expect(bowling.score.length).toEqual(10)
   });
 
+  it('should know the frame', function(){
+    expect(bowling.frame).toEqual(1)
+    bowling.bowl()
+    bowling.bowl()
+    expect(bowling.frame).toEqual(2)
+  });
+
+  it('should know the frameturn', function(){
+    expect(bowling.frameTurn).toEqual(1)
+    bowling.bowl()
+    expect(bowling.frameTurn).toEqual(2)
+  });
+
   it('should randomly generate a number for a bowl and write to the score array', function(){
     bowling.bowl(7)
     expect(bowling.score[0][0]).toEqual(7)
   });
 
-  it('should have 10 pins remaining at start', function(){
-    expect(bowling.pinsRemaining).toEqual(10)
+  it('can write the score in the second frame turn', function() {
+    bowling.bowl(2)
+    bowling.bowl(2)
+    expect(bowling.score[0][1]).toEqual(2)
+  })
+
+  it('should update pins remaining after a turn', function(){
+    bowling.bowl(2)
+    expect(bowling.pinsRemaining).toEqual(8)
   });
 
-  it('should move onto next frameTurn after bowl', function(){
-    bowling.bowl()
-    expect(bowling.frameTurn).toEqual(2)
-  });
-
-  it('should move onto next frameNumber after 2 bowls', function(){
+  xit('should move onto next frameNumber after 2 bowls', function(){
     bowling.bowl()
     bowling.bowl()
-    expect(bowling.frameNumber).toEqual(2)
+    expect(bowling.frame).toEqual(2)
     expect(bowling.frameTurn).toEqual(1)
   });
 
-  it('should have 3 pins remaining after bowling 7', function(){
+  xit('should have 3 pins remaining after bowling 7', function(){
     bowling.bowl(7)
     expect(bowling.pinsRemaining).toEqual(3)
   });
 
-  it('pins remaining should revert at next frame', function(){
+  xit('pins remaining should revert at next frame', function(){
     bowling.bowl()
     bowling.bowl()
     expect(bowling.pinsRemaining).toEqual(10)
   });
 
-  it('should have a total score', function(){
+  xit('should have a total score', function(){
     bowling.bowl(6)
     bowling.bowl(3)
     bowling.bowl(9)
     expect(bowling.totalScore).toEqual(18)
   });
 
-  it('should acknowledge a strike', function(){
+  xit('should acknowledge a strike', function(){
     bowling.bowl(10)
     expect(bowling.frameNumber).toEqual(2)
     expect(bowling.frameTurn).toEqual(1)
   });
 
-  it('should calculate a strike score', function(){
+  xit('should calculate a strike score', function(){
     bowling.bowl(10)
     bowling.bowl(7)
     bowling.bowl(2)
     bowling.applyBonus()
     expect(bowling.score[0][0]).toEqual(19)
+  });
+
+  xit('should calculate a spare score', function(){
+
   });
 
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,57 +1,79 @@
 function Bowling() {
 
   this.score = [[],[],[],[],[],[],[],[],[],[]];
-  this.frameNumber = 1;
-  this.frameTurn = 1;
-  this.pinsRemaining = 10;
-  this.totalScore = 0;
+  this.frame = 1
+  this.frameTurn = 1
+  this.pinsRemaining = 10
 
 };
 
 Bowling.prototype.bowl = function(turn) {
   pinsStruck = turn || Math.floor((Math.random()*this.pinsRemaining) + 0);
-  this.pinsRemaining = this.pinsRemaining - pinsStruck;
-  this.score[this.frameNumber - 1][this.frameTurn - 1] = turn || pinsStruck;
-  this.frameTurn = this.frameTurn + 1;
-  this.totalScore = this.totalScore + pinsStruck;
-  if (this.frameTurn === 3)
-  { this.nextFrame() }
-  if (this.pinsRemaining === 0 && this.frameTurn === 2)
-  { this.nextFrame() }
-};
+  this.writeToScore(pinsStruck)
+  this.updateTurn()
+  this.updatePinsCount(pinsStruck)
+}
 
-Bowling.prototype.nextFrame = function() {
-  this.frameTurn = 1;
-  this.frameNumber = this.frameNumber + 1;
-  this.pinsRemaining = 10;
-};
+Bowling.prototype.updateTurn = function() {
+  this.frameTurn += 1
+  if (this.frameTurn > 2) {
+    this.frame += 1
+  }
+}
 
-Bowling.prototype.applyBonus = function() {
-  if (this.score[0][0] === 10) {
-    this.score[0][0] = this.score[0][0] + this.score[1][0] + this.score[1][1]
-  }
-  if (this.score[1][0] === 10) {
-    this.score[1][0] = this.score[1][0] + this.score[2][0] + this.score[2][1]
-  }
-  if (this.score[2][0] === 10) {
-    this.score[2][0] = this.score[2][0] + this.score[3][0] + this.score[3][1]
-  }
-  if (this.score[3][0] === 10) {
-    this.score[3][0] = this.score[3][0] + this.score[4][0] + this.score[4][1]
-  }
-  if (this.score[4][0] === 10) {
-    this.score[4][0] = this.score[4][0] + this.score[5][0] + this.score[5][1]
-  }
-  if (this.score[5][0] === 10) {
-    this.score[5][0] = this.score[5][0] + this.score[6][0] + this.score[6][1]
-  }
-  if (this.score[6][0] === 10) {
-    this.score[6][0] = this.score[6][0] + this.score[7][0] + this.score[7][1]
-  }
-  if (this.score[7][0] === 10) {
-    this.score[7][0] = this.score[7][0] + this.score[8][0] + this.score[8][1]
-  }
-  if (this.score[8][0] === 10) {
-    this.score[8][0] = this.score[8][0] + this.score[9][0] + this.score[9][1]
-  }
-};
+Bowling.prototype.writeToScore = function(pinsStruck) {
+  this.score[this.frame - 1][this.frameTurn - 1] = pinsStruck;
+}
+
+Bowling.prototype.updatePinsCount = function(pinsStruck) {
+  this.pinsRemaining -= pinsStruck
+}
+
+// Bowling.prototype.bowl = function(turn) {
+//   pinsStruck = turn || Math.floor((Math.random()*this.pinsRemaining) + 0);
+//   this.pinsRemaining = this.pinsRemaining - pinsStruck;
+//   this.score[this.frameNumber - 1][this.frameTurn - 1] = turn || pinsStruck;
+//   this.frameTurn = this.frameTurn + 1;
+//   this.totalScore = this.totalScore + pinsStruck;
+//   if (this.frameTurn === 3)
+//   { this.nextFrame() }
+//   if (this.pinsRemaining === 0 && this.frameTurn === 2)
+//   { this.nextFrame() }
+// };
+
+// Bowling.prototype.nextFrame = function() {
+//   this.frameTurn = 1;
+//   this.frameNumber = this.frameNumber + 1;
+//   this.pinsRemaining = 10;
+// };
+
+// Bowling.prototype.applyBonus = function() {
+//   // STRIKES
+//   if (this.score[0][0] === 10) {
+//     this.score[0][0] = this.score[0][0] + this.score[1][0] + this.score[1][1]
+//   }
+//   if (this.score[1][0] === 10) {
+//     this.score[1][0] = this.score[1][0] + this.score[2][0] + this.score[2][1]
+//   }
+//   if (this.score[2][0] === 10) {
+//     this.score[2][0] = this.score[2][0] + this.score[3][0] + this.score[3][1]
+//   }
+//   if (this.score[3][0] === 10) {
+//     this.score[3][0] = this.score[3][0] + this.score[4][0] + this.score[4][1]
+//   }
+//   if (this.score[4][0] === 10) {
+//     this.score[4][0] = this.score[4][0] + this.score[5][0] + this.score[5][1]
+//   }
+//   if (this.score[5][0] === 10) {
+//     this.score[5][0] = this.score[5][0] + this.score[6][0] + this.score[6][1]
+//   }
+//   if (this.score[6][0] === 10) {
+//     this.score[6][0] = this.score[6][0] + this.score[7][0] + this.score[7][1]
+//   }
+//   if (this.score[7][0] === 10) {
+//     this.score[7][0] = this.score[7][0] + this.score[8][0] + this.score[8][1]
+//   }
+//   if (this.score[8][0] === 10) {
+//     this.score[8][0] = this.score[8][0] + this.score[9][0] + this.score[9][1]
+//   }
+// };
